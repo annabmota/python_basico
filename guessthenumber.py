f47dda55c3e5d9d50dbcfce25b4f61e7ef9fee7a
annabmota <annaabelleemotaa@gmail.com>
2025-11-02 19:00:45 +0100

    Modo multijugador: terminado

--- guessthenumber.py
+++ guessthenumber.py
@@ -2,7 +2,9 @@
 def valida(minimo, maximo):
     opcion = minimo - 1
     while opcion < minimo or opcion > maximo:
-        opcion = int(input(f"Escribe una opción {minimo} y {maximo}: ")) # incluir mensaje si la dificultad no es correcta
+        opcion = int(input(f"Elige una opción entre {minimo} y {maximo}: "))
+        if opcion < minimo or opcion > maximo:
+            opcion = int(input(f"Opción no válida. Debe estar entre {minimo} y {maximo}."))
     return opcion
 
 # Menu de dificultad
@@ -21,7 +23,7 @@ def submenu():
 import random as rdm
 def modo_solitario():
     intentos = submenu()
-    numero_a_adivinar = rdm.randint(1, 1000)
+    numero_a_adivinar = rdm.randint(1, 1000) # Validar que el número esté entre 1 y 1000
     for i in range(intentos):
         numero_introducido = int(input("Adivina el número entre 1 y 1000: "))
         if numero_introducido < numero_a_adivinar:
@@ -32,4 +34,22 @@ def modo_solitario():
             print(f"🎉 ¡Has adivinado el número en {i+1} intentos!")
             return
     else:
-        print(f"😢 Se acabaron los intentos. El número era {numero_a_adivinar}.")        
+        print(f"😢 Se acabaron los intentos. El número era {numero_a_adivinar}.")
+        return     
+    
+# Modo multijugador
+def modo_multijugador():
+    intentos = submenu()
+    numero_a_adivinar_jugador1 = int(input(("Jugador 1, introduce el número a adivinar (entre 1 y 1000): "))) # Hacer que los números no se vean al escribir
+    for i in range(intentos):
+        numero_introducido_jugador2 = int(input("Jugador 2, adivina el número entre 1 y 1000: "))
+        if numero_introducido_jugador2 < numero_a_adivinar_jugador1:
+            print("El número es mayor.")
+        elif numero_introducido_jugador2 > numero_a_adivinar_jugador1:
+            print("El número es menor.")
+        else:
+            print(f"🎉 ¡Has adivinado el número en {i+1} intentos!")
+            return
+    else:
+        print(f"😢 Se acabaron los intentos. El número era {numero_a_adivinar_jugador1}.")
+        return
